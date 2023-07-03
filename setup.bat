62daef7af3398af91096a833a8998409ecae6ff5
Abhishek Pandey <ajaxpandey1234@gmail.com>
2023-07-03 14:21:00 +0545

    Initial Commit

---
+++ setup.bat
@@ -0,0 +1,26 @@
+@echo off
+
+rem Check if the environment file exists
+if not exist venv (
+
+    rem Create the environment
+    python -m venv venv
+
+)
+
+rem Activate the environment
+call venv\Scripts\activate
+
+rem Check if the requirements are installed
+if not exist requirements.txt (
+
+    echo "No requirements file found."
+    exit /b
+
+)
+
+rem Install the requirements
+pip install -r requirements.txt
+
+rem Deactivate the environment
+call venv\Scripts\deactivate
