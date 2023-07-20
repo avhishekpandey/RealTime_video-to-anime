98f93833bbf68729fc1732136e182d2e1d031ae1
Abhishek Pandey <ajaxpandey1234@gmail.com>
2023-07-20 18:46:27 +0545

    removed launch scripts

--- utils.py
+++ utils.py
@@ -1,21 +1,21 @@
 import os
 
 def get_modelpaths(model_name: str):
-  """
-  Get the paths of all the checkpoints for the given model name.
+    """
+    Get the paths of all the checkpoints for the given model name.
 
-  Args:
+    Args:
     model_name: The name of the model.
 
-  Returns:
+    Returns:
     A list of paths to the checkpoints.
-  """
+    """
 
-  checkpoints_dir = os.path.join("Checkpoints", model_name)
-  if not os.path.exists(checkpoints_dir):
-    return []
+    checkpoints_dir = os.path.join("Checkpoints", model_name)
+    if not os.path.exists(checkpoints_dir):
+        return []
 
-  checkpoints = os.listdir(checkpoints_dir)
-  return [os.path.join(checkpoints_dir, checkpoint) for checkpoint in checkpoints]
+    checkpoints = os.listdir(checkpoints_dir)
+    return [os.path.join(checkpoints_dir, checkpoint) for checkpoint in checkpoints]
 
 
