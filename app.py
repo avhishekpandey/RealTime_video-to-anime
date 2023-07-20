5769617525209ecb6b1ba7025eec74ed0c8f8f0c
Abhishek Pandey <ajaxpandey1234@gmail.com>
2023-07-20 18:30:08 +0545

    animeGAN code added

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import cv2
+import numpy as np
+from Scripts.models import AnimeGAN
+from utils import get_modelpaths
+
+model_path = get_modelpaths()
+
+def main():
+    st.title("Real-time Anime to Anime Converter")
+    video_capture = cv2.VideoCapture(0)
+
+    if not video_capture.isOpened():
+        st.error("Error: Unable to access the webcam.")
+        return
+
+    convert_to_anime = AnimeGAN(model_path)
+
+    while True:
+        ret, frame = video_capture.read()
+
+        if not ret:
+            st.warning("Warning: Unable to capture frame from the webcam.")
+            continue
+
+
+        anime_frame = convert_to_anime(frame)
+
+        st.image(np.hstack((frame, anime_frame)), channels="BGR")
+
+        if st.button("Exit"):
+            break
+
+    video_capture.release()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,4 @@
+def get_modelpaths(model_name:str):
+    
+
+
